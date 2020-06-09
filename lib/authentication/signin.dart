da29849a1e825bc2de1114359e772378d20a4c19
bluesky93128 <bluesky93128@gmail.com>
2020-06-09 18:07:37 +0800

    add sign in page

--- lib/authentication/signin.dart
+++ lib/authentication/signin.dart
@@ -8,6 +8,7 @@ class SignInScreen extends StatefulWidget {
 
 class _SignInScreenState extends State<SignInScreen> {
   bool bShowPassword = false;
+  bool rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +24,12 @@ class _SignInScreenState extends State<SignInScreen> {
         ),
         child: Column(
           children: <Widget>[
-            // Image.asset(
-            //   'lib/assets/img/loginbg.png',
-            //   width: MediaQuery.of(context).size.width,
-            //   height: MediaQuery.of(context).size.height,
-            //   fit: BoxFit.fill,
-            // ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.only(top: 160),
+                  padding: EdgeInsets.only(top: 150),
                   child: Image.asset(
                     'lib/assets/img/logo.png',
                     scale: 0.7,
@@ -64,39 +59,149 @@ class _SignInScreenState extends State<SignInScreen> {
             Padding(
               padding: EdgeInsets.only(top: 30),
               child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  child: TextField(
-                    style: TextStyle(color: Colors.white),
-                    obscureText: !bShowPassword,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      labelText: 'Password',
-                      labelStyle: TextStyle(color: Colors.white),
-                      suffixIcon: IconButton(
-                        icon: bShowPassword
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: TextField(
+                  style: TextStyle(color: Colors.white),
+                  obscureText: !bShowPassword,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    suffixIcon: IconButton(
+                      icon: bShowPassword
+                          ? Icon(
+                              Icons.remove_red_eye,
+                              color: Colors.white,
+                            )
+                          : Icon(
+                              OMIcons.removeRedEye,
+                              color: Colors.white,
+                            ),
+                      onPressed: () {
+                        setState(() {
+                          bShowPassword = !bShowPassword;
+                        });
+                      },
+                    ),
+                  ),
+                  autofocus: false,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      MediaQuery.of(context).size.width * 0.1,
+                      10,
+                      MediaQuery.of(context).size.width * 0.1,
+                      0),
+                  child: GestureDetector(
+                    child: Row(
+                      children: <Widget>[
+                        rememberMe
                             ? Icon(
-                                Icons.remove_red_eye,
+                                Icons.check_box_outline_blank,
                                 color: Colors.white,
                               )
                             : Icon(
-                                OMIcons.removeRedEye,
+                                Icons.check_box,
                                 color: Colors.white,
                               ),
-                        onPressed: () {
-                          setState(() {
-                            bShowPassword = !bShowPassword;
-                          });
-                        },
-                      ),
+                        Text(
+                          'Remember Me',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
                     ),
-                    autofocus: false,
-                  )),
+                    onTap: () {
+                      setState(() {
+                        rememberMe = !rememberMe;
+                      });
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                      0, 10, MediaQuery.of(context).size.width * 0.1, 0),
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 24),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 50,
+                child: RaisedButton(
+                  color: Colors.white,
+                  onPressed: () {},
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                      height: 1,
+                      width: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Sign in using',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                      height: 1,
+                      width: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.39,
+                      height: 50,
+                      child: RaisedButton(child: Text('Facebook'), color: Colors.white, onPressed: () {},),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.39,
+                      height: 50,
+                      child: RaisedButton(child: Text('Google'), color: Colors.white, onPressed: () {},),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
