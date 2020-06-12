4ddf5683029bfb03e585db80efb7eca4740d53be
bluesky93128 <bluesky93128@gmail.com>
2020-06-12 18:12:21 +0800

    add staggeredGridView

--- lib/dashboard/dashboard.dart
+++ lib/dashboard/dashboard.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<List> fetchAlbum(page, category) async {
+  final response = await http.get('https://api.unsplash.com/photos/random?client_id=zu8gZp8_xoBcEwA2Mxg-s6Ky4ghDtrYeBUpyNm_KXC0&count=30&query='+category+'&page='+page.toString());
+
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
 
 class Dashboard extends StatefulWidget {
   @override
@@ -6,6 +20,17 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  Future<List> albumData;
+  int cur_page = 1;
+  String cur_category = 'popular';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    albumData = fetchAlbum(cur_page, cur_category);
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -40,6 +65,7 @@ class _DashboardState extends State<Dashboard> {
                 ],
               ),
             ),
+            Expanded(child: _addAlbum(),)
           ],
         ),
       ),
@@ -91,6 +117,45 @@ class _DashboardState extends State<Dashboard> {
           ),
         ],
       ),
+    );
+  }
+
+  _addAlbum() {
+    return FutureBuilder<List>(
+      future: albumData,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return new Padding(
+            padding: const EdgeInsets.all(4.0),
+            //this is what you actually need
+            child: new StaggeredGridView.count(
+              crossAxisCount: 4, // I only need two card horizontally
+              padding: const EdgeInsets.all(2.0),
+              children: snapshot.data.map<Widget>((item) {
+                //Do you need to go somewhere when you tap on this card, wrap using InkWell and add your route
+                return Card(
+                  semanticContainer: false,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                  ),
+                  child: Image.network(item['urls']['small']),
+                );
+              }).toList(),
+
+              //Here is the place that we are getting flexible/ dynamic card for various images
+              staggeredTiles: snapshot.data
+                  .map<StaggeredTile>((_) => StaggeredTile.fit(2))
+                  .toList(),
+              mainAxisSpacing: 3.0,
+              crossAxisSpacing: 4.0, // add some space
+            ),
+          );
+        } else {
+          return Center(
+              child:
+                  new CircularProgressIndicator()); // If there are no data show this
+        }
+      },
     );
   }
 }
